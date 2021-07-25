68910e1b397030df13c681e86f060a714223b2a4
Varun Kumar <varund29@gmail.com>
2021-07-25 23:46:09 +0530

    test

--- src/base.renderer.ts
+++ src/base.renderer.ts
@@ -22,7 +22,7 @@
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
 */
-import { Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {
   JsonSchema,
   OwnPropsOfRenderer,
